0bde388ecbee89b864e2773dcad36dfcf782e118
aron bjartur <83122615+aronbjartur@users.noreply.github.com>
2025-02-20 23:14:07 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,21 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
-import { router } from './routes.js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url'; // Import fileURLToPath from node:url
+import { getDatabase } from './lib/db.client.js';
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-
-const viewsPath = new URL('./views', import.meta.url).pathname;
+// Use fileURLToPath to get the __dirname equivalent in ES modules.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const viewsPath = path.join(__dirname, 'views');
 app.set('views', viewsPath);
 app.set('view engine', 'ejs');
 
-app.use('/', router);
+app.use('/', require('./routes.js').default);
 
 const hostname = '127.0.0.1';
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
