d09cb0d6812c90b095685065ec1afa277e54c7fb
aron bjartur <83122615+aronbjartur@users.noreply.github.com>
2025-02-20 23:15:59 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url'; // Import fileURLToPath from node:url
+import { fileURLToPath } from 'node:url';
 import { getDatabase } from './lib/db.client.js';
 
 const app = express();
 
-// Use fileURLToPath to get the __dirname equivalent in ES modules.
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const viewsPath = path.join(__dirname, 'views');
+// Use alternative variable names to avoid duplicate declarations
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+const viewsPath = path.join(_dirname, 'views');
+
 app.set('views', viewsPath);
 app.set('view engine', 'ejs');
 
-app.use('/', require('./routes.js').default);
+app.use('/', (await import('./routes.js')).default);
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
